0d89e2deac6cf2fee32ba08edde679134db76afb
thiotj <martijn.thio@gmail.com>
2018-10-04 16:34:27 +0200

    Matlab code for the VTS

---
+++ VTS_MatLab/Experiments/stimAll.m
@@ -0,0 +1,32 @@
+function stimAll(session, outputSignal, nrOutputs, serialport)
+% Function that stimulates all the outputs
+% 
+% - session: DAQ session
+% - outputSignal: The output signal
+% - nrOutputs: Number of stimulators
+% - serialport: Serial port
+
+switch nargin
+    case 3
+        serialport = 0;
+end
+
+signals = outputSignal;
+
+for out = 1 : nrOutputs-1
+    signals = cat(2, signals, outputSignal);
+end
+
+queueOutputData(session, repmat(signals, 1, 1));
+disp('stimulating all');
+
+if serialport ~= 0
+    fprintf(serialport, '%c', 110);
+end
+
+session.startForeground;
+
+if serialport ~= 0
+    fprintf(serialport, '%c', 111);
+end
+
---
+++ VTS_MatLab/Experiments/stimSeq.m
@@ -0,0 +1,72 @@
+function stimSeq(session, outputSignal, nrOutputs, serialport, reversals, pTime)
+% Function that stimulates the outputs in sequence
+% 
+% - session: DAQ session
+% - outputSignal: The output signal
+% - nrOutputs: Number of stimulators
+% - serialport: Serial port
+% - reversals: At what point stimulators should vibrate in reverse order
+%   (should be a divisor of the nrOutput), will probably be equal to
+%   fingers
+% - pTime: Pause between stimulation
+
+switch nargin
+    case 3
+        serialport = 0;
+        reversals = nrOutputs;
+        pTime = 0;
+    case 4
+        reversals = nrOutputs;
+        pTime = 0;
+    case 5
+        pTime = 0;
+end
+
+lenOutSig = length(outputSignal);
+out = 1;
+reps = reversals;
+
+if mod(nrOutputs, reversals) == 0
+    while reps <= nrOutputs
+        for i = out:reps
+            allOutputs = zeros(lenOutSig, nrOutputs);
+            allOutputs(:, i) = outputSignal;
+            queueOutputData(session, repmat(allOutputs, 1, 1));
+            disp(strcat('stimulator nr. ', int2str(i)));
+            
+            % send signal through serial port if available
+            if serialport ~= 0 
+                fprintf(serialport, '%c', i);
+            end
+            % stimulate
+            session.startForeground;
+            if serialport ~= 0
+                fprintf(serialport, '%c', 41);
+            end
+            pause(pTime);
+        end
+
+        for i = 0 : reversals-1
+            allOutputs = zeros(lenOutSig, nrOutputs);
+            allOutputs(:, reps - i) = outputSignal;
+            queueOutputData(session, repmat(allOutputs, 1, 1));
+            disp(strcat('stimulator nr. ', int2str(i+1)));
+            
+            % send signal through serial port if available
+            if serialport ~= 0
+                fprintf(serialport, '%c', reps - i);
+            end
+            % stimulate
+            session.startForeground;
+            if serialport ~= 0
+                fprintf(serialport, '%c', 41);
+            end
+            pause(pTime);
+        end
+        out = out + reversals;
+        reps = reps + reversals;
+    end
+else
+    msg = 'Number of outputs is not divisible by number of reversals';
+    error(msg);
+end
