83b5514077e597103a16df9fa94af95e6a0dd784
thiotj <martijn.thio@gmail.com>
2018-10-25 16:57:14 +0200

    updated to display when waiting for trigger

--- VTS_MatLab/fmri_trigger.m
+++ VTS_MatLab/fmri_trigger.m
@@ -3,6 +3,7 @@ function fmri_trigger(serialport, message)
 if nargin < 2 || ~ischar(message) 
     message = 'trigger';
 end
+disp('Waiting for trigger from scanner...')
 
 while 1
     if fread(serialport, 1) ==49
