5e348de3f896b48a6ef7a939dfbb0f8f8c0a29a0
thiotj <martijn.thio@gmail.com>
2019-01-04 13:31:55 +0100

    merged createOutput and createSignal

--- VTS_MatLab/createSignal.m
+++ VTS_MatLab/createSignal.m
@@ -1,5 +1,19 @@
-function signal = createSignal(frequency, amplitude, duration)
-values = linspace(0,2*pi * frequency *duration,...
-    duration*1000)';
-signal = amplitude.*sin(values);
+function signal = createSignal(frequency, amplitude, on_duration, off_duration, reps)
+switch nargin
+    case 3
+        off_duration = 0;
+        reps = 1;
+    case 4
+        reps = 1;
+end
+signal = [];
+values = linspace(0,2*pi * frequency *on_duration,...
+    on_duration*1000)';
+signal_on = amplitude.*sin(values);
+signal_off = zeros(off_duration*1000, 1);
+
+for rep = 1:reps
+        signal = [signal;signal_on;signal_off];
+end
+
 end
