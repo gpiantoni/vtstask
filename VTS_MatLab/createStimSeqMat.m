e8973f1532e303ed8bc1560ae342d9e97f235438
thiotj <martijn.thio@gmail.com>
2019-01-04 13:59:51 +0100

    Changed stimSeqMat.m
    stimSeqMat.m is now dependent on stimRandMat.m

--- VTS_MatLab/createStimSeqMat.m
+++ VTS_MatLab/createStimSeqMat.m
@@ -1,8 +1,8 @@
-function seqMat = createStimSeqMat(nrOutputs, signal, reps, restDur, pTime, reversed, serialport)
+function seqMat = createStimSeqMat(nrOutputs, signal, blocks, restDur, pTime, reversed, serialport)
 
 switch nargin
     case 2
-        reps = 2;
+        blocks = 2;
         restDur = 5;
         pTime = 3;
         reversed = false;
@@ -23,39 +23,21 @@ switch nargin
         serialport = 0;
 end
 
-stimDur = length(signal)/1000;
-seqMat = [];
-for rep = 1:reps
-    if rep == 1
-        rest = zeros(restDur*1000,1);
-    else
-        rest = zeros((restDur-stimDur)*1000,1);
-    end
-    
+sigVect = [];
+for i = 1:blocks
+    order = [];
     if ~reversed
-        allSignals = [];
-        for finger = 1:nrOutputs
-            before = (finger-1)*pTime;
-            after = (nrOutputs-finger)*pTime;
-            tbs = zeros(int64(before*1000),1);
-            tas = zeros(int64(after*1000), 1);
-            sigVector = [rest;tbs;signal;tas];
-            allSignals = [allSignals sigVector];
+        for output = 1:nrOutputs
+            order = [order, output];
         end
-        seqMat = [seqMat; allSignals];
-        reversed = ~reversed;
     else
-        allSignalsRev = [];
-        for finger = 1:nrOutputs
-            before = (nrOutputs-finger)*pTime;
-            after = (finger-1)*pTime;
-            tbs = zeros(int64(before*1000),1);
-            tas = zeros(int64(after*1000), 1);
-            sigVector = [rest;tbs;signal;tas];
-            allSignalsRev = [allSignalsRev sigVector];
+        for output = 1:nrOutputs
+            order = [order, (nrOutputs+1)-output];
         end
-        seqMat = [seqMat; allSignalsRev];
-        reversed = ~reversed;
     end
+    sigVect = [sigVect;order];
+    reversed = ~reversed;
 end
+
+seqMat = createStimRandMat(nrOutputs, signal, sigVect, restDur, pTime, serialport);
 end
