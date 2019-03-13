403fcacbdbfc01f651e3b74e2f1a810c518c28af
thiotj <martijn.thio@gmail.com>
2019-03-13 12:24:31 +0100

    Update VTS_Design.m

--- VTS_MatLab/VTS_Design.m
+++ VTS_MatLab/VTS_Design.m
@@ -1,5 +1,5 @@
-clear
-subject = 'test_3';
+clear;clc
+subject = 'V8635';
 logdir = 'C:\Users\mthio.LA020080\Documents\GitHub\vtstask\logfiles\';
 logfile = fopen(strcat(logdir, 'log_subject_', subject, '.txt'), 'a');
 logger(logfile, char(strcat('Initiate logging for subject', {' '}, subject))); 
@@ -18,29 +18,40 @@ daq2 = 'cDAQ1mod2';
 %% Create output signal
 nrOutputs = 5;
 amplitude = 2; %TODO: Find out what the output amplitude is (in voltage)
-frequency = 30 ; %Hz
+frequency = 30;
+frequency2 = 110; %Hz
+frequency3 = 190;
 % stimulation variables for stimAll
 stimdur_all_on = .2;
 stimdur_all_off = .05;
-reps_all = 2;
+stimdur_total_all = 16;
+reps_all = stimdur_total_all/(stimdur_all_on+stimdur_all_off);
+
 % stimulation variables seperate finger stimulation
 stimdur_fing_on = .4;
 stimdur_fing_off = .1;
-restdur = 14.4;  
-pTime = 6.4;
-reps_fing = 8;
+stimdur_total_fing = 4;
+
+restdur = 14.4; % Rest between stimulation (after stimulation stops)
+% restTime = restdur + stimdur_total_fing;
+pDur = 10;
+pTime = pDur + stimdur_total_fing;
+reps_fing = stimdur_total_fing/(stimdur_fing_on+stimdur_fing_off) ;
 
 % create signals for both
 signal_all = createSignal(frequency, amplitude, stimdur_all_on, stimdur_all_off, reps_all);
 signal_fing = createSignal(frequency, amplitude, stimdur_fing_on, stimdur_fing_off, reps_fing);
+signal_fing2 = createSignal(frequency2, amplitude, stimdur_fing_on, stimdur_fing_off, reps_fing);
+signal_fing3 = createSignal(frequency3, amplitude, stimdur_fing_on, stimdur_fing_off, reps_fing);
 
 % --- Onsets based on hrf_pattern ---
 hrf_filename = 'sub-V7818_ses-UMC-7T_task-hrfpattern_run-1_20180306T170321.tsv';
 hrf_tsv = tdfread(hrf_filename);
 hrf_onsets =  hrf_tsv.onset;
+onsets = [16, 48, 80, 112, 144, 172, 204, 236];  % klaar met stimuleren na 252 sec
 
 % --- read file with randomised stimulations ---
-rand_filename = 'randomlist_5_fingers.txt';
+rand_filename = 'randomfingers.txt';
 randFile = fopen(rand_filename);
 randcell = textscan(randFile, '%d');
 randomlist = cell2mat(randcell);
@@ -50,7 +61,7 @@ orderlist = transpose(reshape(randomlist, [nrOutputs,randlength]));
 
 %% log files for experiments
 fprintf(logfile, '\n%s: %s\n%s: %s\n\n',...
-    'hRF onsets file: ', hrf_filename, ...
+    'hRF onsets: ', num2str(onsets), ...
     'random stimulation filename: ', rand_filename); 
 %% add all the channels to the session
 %addChannels(s, nrOutputs, daq1)
@@ -70,7 +81,7 @@ try
     sp_in = serial(PORT_IN);
     fopen(sp_in);
     disp(strcat('opening serial port:', {' '}, PORT_IN));
-    sp_in.Timeout = 300;
+    sp_in.Timeout = inf;
 catch ME
     warning('no input serial port found. input serial port set to 0');
     sp_in = 0;
@@ -80,9 +91,9 @@ end
 try
     %% --- stimulate all ---
     logger(logfile, 'INPUTS STIMULATE ALL:', 0);
-    logvars(logfile, nrOutputs, amplitude, frequency, stimdur_all_on, stimdur_all_off);
+    logvars(logfile, nrOutputs, amplitude, frequency, stimdur_all_on, stimdur_all_off, stimdur_total_all);
     
-    stimAllMat = createStimAllMat(nrOutputs, signal_all, hrf_onsets(1:1));
+    stimAllMat = createStimAllMat(nrOutputs, signal_all, onsets);
 
     queueOutputData(s, stimAllMat)
     if sp_in ~=0
@@ -91,40 +102,16 @@ try
     tic
     s.startBackground
     logger(logfile, char(strcat('start time delay is', {' '}, num2str(toc))))
-    logall(logfile, hrf_onsets(1:1));
+    logall(logfile, onsets);
     if sp_in ~=0
         fmri_trigger(sp_in, logfile, 'End of stimulating all outputs')
         stop(s);
     end
-    %% --- stimulate sequential ---
-    blocks = 4;
-    seqmat = createSeqMat(nrOutputs, blocks);
-    stimSeqMat = createStimFingMat(nrOutputs, signal_fing, seqmat, restdur, pTime);
-    
-    logger(logfile, 'INPUTS STIMULATE IN SEQUENCE:', 0)
-    logvars(logfile, nrOutputs, amplitude, frequency, stimdur_fing_on, stimdur_fing_off, ...
-        restdur, pTime, blocks);
-    
-    queueOutputData(s, stimSeqMat)
-   
-    if sp_in ~=0
-        fmri_trigger(sp_in, logfile, 'Start stimulating ouputs in sequence')
-    end
-    
-    tic
-    s.startBackground
-    logger(logfile, char(strcat('start time delay is', {' '}, num2str(toc))))
-    logfing(logfile, seqmat, restdur, pTime);
-    
-    if sp_in ~=0
-        fmri_trigger(sp_in, logfile, 'End of stimulating ouputs in sequence');
-        stop(s);
-    end
-    %% --- stimulate random ---
+    %% --- stimulate random #1---
     randmat = createStimFingMat(nrOutputs, signal_fing, orderlist, restdur, pTime);
-    logger(logfile, 'INPUTS STIMULATE IN RANDOM ORDER:', 0)
+    logger(logfile, 'INPUTS STIMULATE IN RANDOM ORDER #1 :', 0)
     logvars(logfile, nrOutputs, amplitude, frequency, stimdur_fing_on, stimdur_fing_off, ...
-        restdur, pTime, randlength);
+        stimdur_total_fing, restdur, pDur, randlength);
     
     queueOutputData(s, randmat)
     
@@ -135,7 +122,50 @@ try
     tic
     s.startBackground
     logger(logfile, char(strcat('start time delay is', {' '}, num2str(toc))))
-    logfing(logfile, orderlist, restdur, pTime);
+    logfing(logfile, orderlist, restdur, pTime, stimdur_total_fing);
+    
+    if sp_in ~=0
+        fmri_trigger(sp_in, logfile, 'End of stimulating outputs in random order')
+        stop(s);
+    end
+    %% --- stimulate random #2---
+    randmat = createStimFingMat(nrOutputs, signal_fing2, orderlist, restdur, pTime);
+    logger(logfile, 'INPUTS STIMULATE IN RANDOM ORDER #2 :', 0)
+    logvars(logfile, nrOutputs, amplitude, frequency2, stimdur_fing_on, stimdur_fing_off, ...
+        stimdur_total_fing, restdur, pDur, randlength);
+    
+    queueOutputData(s, randmat)
+    
+    if sp_in ~=0
+        fmri_trigger(sp_in, logfile, 'Start stimulating outputs in random order')
+    end
+    
+    tic
+    s.startBackground
+    logger(logfile, char(strcat('start time delay is', {' '}, num2str(toc))))
+    logfing(logfile, orderlist, restdur, pTime, stimdur_total_fing);
+    
+    if sp_in ~=0
+        fmri_trigger(sp_in, logfile, 'End of stimulating outputs in random order')
+        stop(s);
+    end
+    
+%     %% --- stimulate random #3---
+    randmat = createStimFingMat(nrOutputs, signal_fing3, orderlist, restdur, pTime);
+    logger(logfile, 'INPUTS STIMULATE IN RANDOM ORDER #3 :', 0)
+    logvars(logfile, nrOutputs, amplitude, frequency3, stimdur_fing_on, stimdur_fing_off, ...
+        stimdur_total_fing, restdur, pDur, randlength);
+    
+    queueOutputData(s, randmat)
+    
+    if sp_in ~=0
+        fmri_trigger(sp_in, logfile, 'Start stimulating outputs in random order')
+    end
+    
+    tic
+    s.startBackground
+    logger(logfile, char(strcat('start time delay is', {' '}, num2str(toc))))
+    logfing(logfile, orderlist, restdur, pTime, stimdur_total_fing);
     
     if sp_in ~=0
         fmri_trigger(sp_in, logfile, 'End of stimulating outputs in random order')
@@ -169,16 +199,17 @@ catch e
     fprintf(2, e.message);
 end
 
-function logvars(logfile, nrOutputs, amplitude, frequency, stimdur_on, stimdur_off, restdur, pTime, reps)
-if nargin < 7
-    fprintf(logfile, '\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n\n',...
+function logvars(logfile, nrOutputs, amplitude, frequency, stimdur_on, stimdur_off, stimdur_total, restdur, pTime, reps)
+if nargin < 8
+    fprintf(logfile, '\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n\n',...
         'number of outputs: ', nrOutputs,...
         'amplitude: ', amplitude, ...
         'frequency: ', frequency, ...
         'duration of stimulation ON in seconds: ', stimdur_on, ...
-        'duration of stimulation OFF in seconds: ', stimdur_off);
+        'duration of stimulation OFF in seconds: ', stimdur_off, ...
+        'total stimulation duration: ', stimdur_total);
 else
-    fprintf(logfile, '\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n\n',...
+    fprintf(logfile, '\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n\n',...
         'number of outputs: ', nrOutputs,...
         'amplitude: ', amplitude, ...
         'frequency: ', frequency, ...
@@ -186,6 +217,7 @@ else
         'duration of stimulation OFF in seconds: ', stimdur_off, ...
         'duration between blocks in seconds: ', restdur,...
         'duration between stimulation in seconds: ', pTime,...
+        'total stimulation duration: ', stimdur_total, ... 
         'number of repetitions: ', reps);
 end
 end
