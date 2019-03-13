d51f9d613d03680f074f48bc401307cd9070e391
thiotj <martijn.thio@gmail.com>
2019-03-13 12:22:02 +0100

    edited createStimFingMat

--- VTS_MatLab/createStimFingMat.m
+++ VTS_MatLab/createStimFingMat.m
@@ -1,4 +1,4 @@
-function stimRandMat = createStimRandMat(nrOutputs, signal, orderlist, restDur, pTime, serialport)
+function stimRandMat = createStimFingMat(nrOutputs, signal, orderlist, restDur, pTime, serialport)
 
 switch nargin
     case 3
@@ -22,18 +22,25 @@ for rep = 1:reps
     if rep == 1
         rest = zeros(restDur*1000,1);
     else
-        rest = zeros((restDur-stimDur)*1000,1);
+        rest = zeros((restDur)*1000,1);
     end
-    
     allSignals = [];
     order = orderlist(rep,:);
     for output = 1:nrOutputs
+        sigVector = 0;
         pos = find(order==output);
-        before = (pos-1)*pTime;
-        after = (5-pos)*pTime;
-        tbs = zeros(int64(before*1000),1);
-        tas = zeros(int64(after*1000), 1);
-        sigVector = [rest;tbs;signal;tas];
+        if isempty(pos)
+            nosig = stimDur+pTime*(nrOutputs-1);
+            sigVector = [rest;zeros(int64(nosig*1000), 1)];
+        else
+            for p = 1:length(pos)
+                before = (pos(p)-1)*pTime;
+                after = (5-pos(p))*pTime;
+                tbs = zeros(int64(before*1000),1);
+                tas = zeros(int64(after*1000), 1);
+                sigVector = sigVector + [rest;tbs;signal;tas];
+            end
+        end
         allSignals = [allSignals sigVector];
     end
     stimRandMat = [stimRandMat;allSignals];
